2d94739f272d2febc9b5bb32ce1261864956b850
Rodrigo Nakayama <57539031+RodrigoNakayama@users.noreply.github.com>
2026-08-06 09:07:29 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var items = [
+    {
+        title : "Super Mario World DETONADO",
+        content : "detonado do clássico jogo de super nintendo"
+    },
+    {
+        title : "Como fazer cannabis usando maizena e javascript",
+        content : "Tutorial de uma prática receita de vó"
+    },
+    {
+        title : "PodCast a respeito de cálculo e matemática avançada",
+        content : "PodCast acompanhado do grande mestre Ledo Vaccaro discutindo a respeito de cálculo"
+    }
+]
+
+const Coisa = document.getElementById('container')
+
+const Thing = document.createElement("div")
+
+const textin = document.createElement("h2")
+h2.textContent = "bananana"
+
+console.log(Coisa)
